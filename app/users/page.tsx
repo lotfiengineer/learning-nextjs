8d29415c4560a1559329836fd04abfceef091632
Mohammad Lotfi <mohammad.lotfi7913@gmail.com>
2025-01-27 16:15:28 +0330

    showing loading ui

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import UserTable from "./UserTable";
+import Link from "next/link";
 
 interface Props {
   searchParams: { sortOrder: string };
 }
 
 const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
-  console.log(sortOrder);
-
   return (
     <>
       <h1>Users</h1>
-      <UserTable sortOrder={sortOrder} />
+      <Link href={"/users/new"} className="btn">
+        New User
+      </Link>
+      <Suspense fallback={<p>Loading here...</p>}>
+        <UserTable sortOrder={sortOrder} />
+      </Suspense>
     </>
   );
 };

349c4ea480f6acc21d708cbd8666a5409675a23d
Mohammad Lotfi <mohammad.lotfi7913@gmail.com>
2025-02-02 12:22:48 +0330

    learned deleting an object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 // export function GET(request: NextRequest, {params}: { id: number }) {
@@ -17,4 +18,40 @@ export function GET(
     return NextResponse.json({ error: "User not found" }, { status: 404 });
 
   return NextResponse.json({ id: 1, name: "Mosh" });
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params: { id } }: { params: { id: number } }
+) {
+  // Validate the request body
+  // If invalid, return 400
+  // Fetch the user with the given id
+  // If doesn't exist, return 404
+  // Update the user
+  // Return the updated user
+
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+
+  if (id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: body.name });
+}
+
+export function DELETE(
+  request: NextRequest,
+  { params: { id } }: { params: { id: number } }
+) {
+  // Fetch user from db
+  // if not found, return 404
+  // delete the user
+  // return 200
+
+  if (id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({});
 }

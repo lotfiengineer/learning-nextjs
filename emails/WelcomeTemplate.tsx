33923bf6782a28d2342b9309a6ff6495ebcc69a0
Mohammad Lotfi <mohammad.lotfi7913@gmail.com>
2025-02-11 11:38:16 +0330

    learned sending emails

---
+++ emails/WelcomeTemplate.tsx
@@ -0,0 +1,29 @@
+import React, { CSSProperties } from "react";
+import {
+  Html,
+  Body,
+  Container,
+  Text,
+  Tailwind,
+  Link,
+  Preview,
+} from "@react-email/components";
+
+const WelcomeTemplate = ({ name }: { name: string }) => {
+  return (
+    <Html>
+      <Preview>Welcome aboard!</Preview>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl">Hello {name}</Text>
+            <Link href="https://lotfiengineer.com">www.lotfiengineer.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
+
+export default WelcomeTemplate;
+
